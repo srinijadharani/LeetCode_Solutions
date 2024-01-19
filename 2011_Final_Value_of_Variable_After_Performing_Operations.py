82c08bed150b24ea373220ba32a71731c84fb4e6
Srinija <61744464+srinijadharani@users.noreply.github.com>
2024-01-19 13:41:22 -0500

    Create 2011_Final_Value_of_Variable_After_Performing_Operations.py

---
+++ 2011_Final_Value_of_Variable_After_Performing_Operations.py
@@ -0,0 +1,9 @@
+class Solution:
+    def finalValueAfterOperations(self, operations: List[str]) -> int:
+        X = 0
+        for i in operations:
+            if "+" in i:
+                X+=1
+            elif "-" in i:
+                X-=1
+        return (X)  
