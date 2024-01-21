4ef6b28a8462f20c19a81b4d9fd1e18e2cf76d50
Srinija <61744464+srinijadharani@users.noreply.github.com>
2024-01-21 17:37:37 -0500

    Create 709_To_Lower_Case.py

---
+++ 709_To_Lower_Case.py
@@ -0,0 +1,5 @@
+class Solution:
+    def toLowerCase(self, s: str) -> str:
+        return s.lower()
+
+# I'm embarrassed that I'm logging this entry like it's an achievement. I'm only doing this so I don't lose streak! :')
